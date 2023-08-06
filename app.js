3ffc6e8c00a827d35cd41485d753816a97751ccd
topus009 <sj_89@mail.ru>
2023-08-06 22:31:09 +0300

    scasac

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 
-const port = process.env.PORT || 5005
+const port = process.env.PORT || 3000
 const service = process.env.SERVICE || "SECTION"
 
 app.get('/', function(req, res) {
